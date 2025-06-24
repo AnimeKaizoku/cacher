68ac84c9255a5baa73d483293b5ff5ea47bf00ac
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2025-06-24 20:58:29 +0530

    feat: Added a centralized cleaner

--- cleaner.go
+++ cleaner.go
@@ -4,23 +4,63 @@ import (
 	"time"
 )
 
+type Cleanable interface {
+	cleanExpired()
+	getCleanInterval() time.Duration
+}
+
+type Cleaner struct {
+	cachers       []Cleanable
+	cleanInterval time.Duration
+}
+
+func NewCleaner() *Cleaner {
+	return &Cleaner{
+		cachers: make([]Cleanable, 0),
+	}
+}
+
+// A function to help find GCD of 2 time Durations
+func gcd(a, b time.Duration) time.Duration {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func (cl *Cleaner) calculateIntervalGCD() {
+	if len(cl.cachers) == 0 {
+		cl.cleanInterval = 0
+		return
+	}
+	interval := cl.cachers[0].getCleanInterval()
+	for _, c := range cl.cachers[1:] {
+		interval = gcd(interval, c.getCleanInterval())
+	}
+	cl.cleanInterval = interval
+}
+
+func (cl *Cleaner) Register(c Cleanable) {
+	cl.cachers = append(cl.cachers, c)
+	cl.calculateIntervalGCD()
+}
+
+func (cl *Cleaner) Run() {
+	go func() {
+		for {
+			for _, c := range cl.cachers {
+				c.cleanExpired()
+			}
+			time.Sleep(cl.cleanInterval)
+		}
+	}()
+}
+
 // A function used by current Cacher instance to clean
 // expired keys on regular basis.
 func (c *Cacher[C, T]) cleaner() {
 	for {
-		currTime := time.Now().Unix()
-		c.mutex.Lock()
-		for key, val := range c.cacheMap {
-			// Skip the current clean window if cacher is reset or deleted.
-			if c.status == cacherReset || c.status == cacherDeleted {
-				c.status = noop
-				break
-			}
-			if val.expiry <= currTime {
-				delete(c.cacheMap, key)
-			}
-		}
-		c.mutex.Unlock()
+		c.cleanExpired()
 		// cleanup expired keys every c.cleanInterval duration
 		time.Sleep(c.cleanInterval)
 	}
